ef06bb4c872e07c2fe5af463f7fd5de19005400c
Jeremie Corpinot <jeremiecorpinot@outlook.com>
2014-02-21 13:52:03 +0100

    Add Jinja2 module
    Added files to use templating with Jinja2
    It's a python script

--- python/myscript.py
+++ python/myscript.py
@@ -1,40 +1,39 @@
 # -*-coding:Latin-1 -*
 
 import os, sys
-# import py_packages.markdown
 from py_packages.markdown import Markdown
-
-# print py_packages.markdown.markdown("*boo!*")
+from jinja2 import Template
 
 markdowner = Markdown()
-# print markdowner.convert("*boo!*")
+print markdowner.convert("*boo!*")
 
-
+template = Template('Hello {{name}}!')
+print template.render(name='John Doe')
 # chemin du dossier d'exercices
-exercices_folder = """exercices/"""
-dirs = os.listdir(exercices_folder)
+# exercices_folder = """exercices/"""
+# dirs = os.listdir(exercices_folder)
 # print dirs
 
 # récupère chaque exercice au format .md
-# et affiche son contenu dans le terminal
-for course in dirs:
-	# print ("\n\n" + course)
-	files = os.listdir(exercices_folder + course)
-	for file in files:
-		# print("\n" + file)
-		chars = file[-3:]
-		# print chars
-		if ".md" in chars:
-			full_path = exercices_folder + course + "/" + file
-			# print full_path
-			with open(full_path, 'r') as exercice_file:
-				text = exercice_file.read()
-				# print markdowner.convert(text)
-				new_path = str.replace(full_path, file, file[:-2] + "html")
-				# print new_path
+# # et affiche son contenu dans le terminal
+# for course in dirs:
+# 	# print ("\n\n" + course)
+# 	files = os.listdir(exercices_folder + course)
+# 	for file in files:
+# 		# print("\n" + file)
+# 		chars = file[-3:]
+# 		# print chars
+# 		if ".md" in chars:
+# 			full_path = exercices_folder + course + "/" + file
+# 			# print full_path
+# 			with open(full_path, 'r') as exercice_file:
+# 				text = exercice_file.read()
+# 				# print markdowner.convert(text)
+# 				new_path = str.replace(full_path, file, file[:-2] + "html")
+# 				# print new_path
 
-				with open(new_path, 'w') as html_file:
-					html_file.write(markdowner.convert(text))
-					print("\nConversion : " + file)
+# 				with open(new_path, 'w') as html_file:
+# 					html_file.write(markdowner.convert(text))
+# 					print("\nConversion : " + file)
 
 os.system('pause')
