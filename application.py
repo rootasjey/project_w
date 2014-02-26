33552ee192eee3d973874718866f8b7a03fa2312
Jeremie Corpinot <jeremiecorpinot@outlook.com>
2014-02-26 04:52:47 +0100

    Re-construction of the directory

---
+++ application.py
@@ -0,0 +1,39 @@
+# -*-coding:Latin-1 -*
+
+import os, sys
+from packages.markdown import Markdown
+from packages.jinja2 import Template
+
+markdowner = Markdown()
+print markdowner.convert("*boo!*")
+
+template = Template('Hello {{name}}!')
+print template.render(name='John Doe')
+# chemin du dossier d'exercices
+# exercices_folder = """exercices/"""
+# dirs = os.listdir(exercices_folder)
+# print dirs
+
+# récupère chaque exercice au format .md
+# # et affiche son contenu dans le terminal
+# for course in dirs:
+# 	# print ("\n\n" + course)
+# 	files = os.listdir(exercices_folder + course)
+# 	for file in files:
+# 		# print("\n" + file)
+# 		chars = file[-3:]
+# 		# print chars
+# 		if ".md" in chars:
+# 			full_path = exercices_folder + course + "/" + file
+# 			# print full_path
+# 			with open(full_path, 'r') as exercice_file:
+# 				text = exercice_file.read()
+# 				# print markdowner.convert(text)
+# 				new_path = str.replace(full_path, file, file[:-2] + "html")
+# 				# print new_path
+
+# 				with open(new_path, 'w') as html_file:
+# 					html_file.write(markdowner.convert(text))
+# 					print("\nConversion : " + file)
+
+os.system('pause')
