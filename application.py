cd27b0e18d09809091db80431e4140b6ac483558
Jeremie Corpinot <jeremiecorpinot@outlook.com>
2014-03-02 01:51:10 +0100

    Jinja2
    Modeling & Templating web pages
    + Added a template (skeleton.html) for the template base
    + Added index's stylesheet (css file)

--- application.py
+++ application.py
@@ -3,14 +3,16 @@
 
 import os, sys
 from packages.jinja2 import Template
-from packages.flask import Flask
+from packages.flask import Flask, request, render_template
 from packages.converter import converter
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return "BIENVENU(E) !"
+	# mots = ["Ceci", "est" "un", "test."]
+	# return render_template('index.html', titre="Bienvenue !", mots=mots)
+	return render_template('index.html')
 
 @app.route('/contact/')
 def contact():
