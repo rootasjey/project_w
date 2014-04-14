51ed68a81d0e7268eabc4be6164d87770e08dfe4
somiak <somia.katir@gmail.com>
2014-04-14 15:50:53 +0200

    le filtre Eval

---
+++ appEval.py
@@ -0,0 +1,19 @@
+
+import os, sys
+
+from packages.converter import converter
+from jinja2 import Template
+from flask import Flask, request, render_template
+
+app = Flask(__name__, template_folder='')
+
+@app.route('/testEval')
+def testEval():
+	return render_template('/Tests/testEval.html')
+@app.template_filter('eval_filtre')
+def Evaluer_args(var1):
+    return eval(var1)
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
